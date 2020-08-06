306261ddd36a92e3b849d00666485e850c560e88
Laerte <laerte_256@hotmail.com>
2020-08-06 20:03:05 -0300

    teste1

--- src/ExClasses.kt
+++ src/ExClasses.kt
@@ -7,6 +7,12 @@ fun main() {
     microfone.diminuirVolume(5)
     microfone.status()
 
+    var cachorro = Cachorro("Mel", true,true)
+    cachorro.dormir()
+    cachorro.moder()
+    cachorro.comer()
+    cachorro.comer()
+    cachorro.moder()
 }
 
 class Microfone(val modelo: String,val marca: String,val conexao :String){
@@ -38,6 +44,32 @@ class Microfone(val modelo: String,val marca: String,val conexao :String){
             println("Microfone desligado")
         } else {
             println("Volume $volume")
+        }
+    }
+
+}
+
+class Cachorro(val nome:String, var fome : Boolean, var bravo :Boolean){
+
+    fun comer (){
+        if(!fome){
+            println("$nome não está com fome")
+        }else{
+            println("$nome não está com fome")
+            println("$nome come")
+            this.fome = false
+            this.bravo = false
+        }
+    }
+    fun dormir(){
+        println("$nome dorme")
+    }
+
+    fun moder(){
+        if (bravo){
+            println("$nome te morde")
+        }else{
+            println("$nome não te morde")
         }
     }
 
